d4ac467417c3bd20bb428b4902f80d36dfe870f2
dengsc <dengsc@jpush.cn>
2018-10-11 17:47:24 +0800

    支持日志实时推送到web

--- app/consumers.py
+++ app/consumers.py
@@ -7,49 +7,26 @@
 # @Software: PyCharm
 
 
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
+from utils.redis_api import RedisQueue
 import json
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+class LogsConsumer(WebsocketConsumer):
+    def connect(self):
+        self.log_id = self.scope['url_route']['kwargs']['log_id']
 
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        print('join: ' + self.log_id)
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        redis = RedisQueue(name=self.log_id)
+        while True:
+            message = redis.get(timeout=10)
+            print('logs: ' + str(message))
+            if message:
+                # Send message to room group
+                print('send: ' + str(message))
+                self.send(text_data=json.dumps({
+                    'message': str(message)
+                }))
--- app/routing.py
+++ app/routing.py
@@ -12,5 +12,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    url(r'^ws/logs/(?P<log_id>[^/]+)/$', consumers.LogsConsumer),
 ]
